7e152fcb331a8790418dd7b0ce7d7cdcc76870ba
spatialhast <a.hast@mail.ru>
2015-09-17 19:46:51 +0300

    add delegate

---
+++ L12/in_class.js
@@ -0,0 +1,28 @@
+$on( qs('#clear-completed', 'click', fname))
+
+$('#listDo').on('click', 'button');
+
+target - родитель на котором хотим отслеживать событие\
+selector - элемент
+type 
+handler
+
+//----------------------------------------------------------------
+
+
+function consoleValue(event) {
+	console.log(event.target.value);
+};
+
+$delegate( qs('body'), 'input[type=text]', 'focus', consoleValue);
+$delegate( qs('body'), 'input[type=text]', 'blur', consoleValue);
+$delegate( qs('body'), 'input[type=text]', 'click', consoleValue);
+
+
+//console.log( qs('input[type=text]') );
+
+//----------------------------------------------------------------
+
+
+
+
